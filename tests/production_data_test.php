5dc7307a7ee8d93553e999b346dcd6ac089aac16
Phil <111912911+philvangaatd@users.noreply.github.com>
2026-08-11 01:38:04 +0200

    Cover production resources in contract tests

--- tests/production_data_test.php
+++ tests/production_data_test.php
@@ -31,16 +31,46 @@ $points = normalize_live_production_points([
                 'status' => 0,
             ],
         ],
-        'storages' => [],
+        'storages' => [
+            [
+                'fillType' => 'WATER',
+                'title' => 'Wasser',
+                'role' => 'input',
+                'level' => 6800,
+                'capacity' => 10000,
+                'percent' => 68,
+            ],
+            [
+                'fillType' => 'TOMATO',
+                'title' => 'Tomaten',
+                'role' => 'output',
+                'level' => 1250,
+                'capacity' => 5000,
+                'percent' => 25,
+            ],
+            [
+                'fillType' => 'STRAWBERRY',
+                'title' => 'Erdbeeren',
+                'role' => 'output',
+                'level' => 400,
+                'capacity' => 5000,
+                'percent' => 8,
+            ],
+        ],
     ],
 ]);
 
 expect_production_test(count($points) === 1, 'Produktionsanlage fehlt.');
 expect_production_test($points[0]['activeCount'] === 1, 'Aktivzähler ist falsch.');
-expect_production_test(count($points[0]['productions']) === 2, 'Produktionsketten fehlen.');
+expect_production_test(count($points[0]['productions']) === 1, 'Es dürfen nur aktive Produktionsketten ausgegeben werden.');
 expect_production_test($points[0]['productions'][0]['label'] === 'Tomaten', 'Produktionsname fehlt.');
 expect_production_test($points[0]['productions'][0]['enabled'] === true, 'Aktive Produktion wurde nicht erkannt.');
-expect_production_test($points[0]['productions'][1]['enabled'] === false, 'Inaktive Produktion wurde falsch aktiviert.');
 expect_production_test($points[0]['productions'][0]['status'] === '2', 'Produktionsstatus wurde nicht stabil normalisiert.');
+expect_production_test($points[0]['water']['fillType'] === 'WATER', 'Wasserbestand fehlt.');
+expect_production_test($points[0]['water']['level'] === 6800, 'Wassermenge ist falsch.');
+expect_production_test(count($points[0]['inputStorages']) === 1, 'Aktiver Betriebsstoff fehlt.');
+expect_production_test(count($points[0]['outputStorages']) === 1, 'Es darf nur das Produkt einer aktiven Produktion erscheinen.');
+expect_production_test($points[0]['outputStorages'][0]['fillType'] === 'TOMATO', 'Produzierte Tomaten fehlen.');
+expect_production_test($points[0]['outputStorages'][0]['level'] === 1250, 'Produzierte Tomatenmenge ist falsch.');
 
 echo "production_data_test: ok\n";
